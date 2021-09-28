61af0de0539cd21d1642e8a261a52408be417da5
JFmaia <jfmaia741@gmail.com>
2021-09-28 09:06:40 -0300

    Ajustes
    Colocando comentarios e ajustando detalhes nas cores dos icones e da caixa de entrada de texto depois que clicada.

--- password_validated/lib/main.dart
+++ password_validated/lib/main.dart
@@ -65,25 +65,40 @@ class _PasswordValidationPageState extends State<PasswordValidationPage> {
             TextField(
               obscureText: !_isVisible,
               decoration: InputDecoration(
-                  //Icone de visualizar a senha.
-                  suffixIcon: IconButton(
-                    icon: _isVisible
-                        ? Icon(Icons.visibility)
-                        : Icon(Icons.visibility_off),
-                    //Mudando o estado do icone de visualização da senha.
-                    onPressed: () {
-                      setState(() {
-                        _isVisible = !_isVisible;
-                      });
-                    },
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: "Senha",
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
+                //Icone de visualizar a senha.
+                suffixIcon: IconButton(
+                  icon: _isVisible
+                      ? Icon(
+                          Icons.visibility,
+                          color: Colors.black,
+                        )
+                      : Icon(
+                          Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                  //Mudando o estado do icone de visualização da senha.
+                  onPressed: () {
+                    setState(() {
+                      _isVisible = !_isVisible;
+                    });
+                  },
+                ),
+                //Forma da caixa de texto e cor.
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                //Forma da caixa de texto e cor, depois de selecionada.
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                //Texto que fica antes da digitação.
+                hintText: "Senha",
+                //Distancia dentro da caixa de entrada de texto.
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              ),
             ),
           ],
         ),
